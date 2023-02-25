09e5588e90f0cba5f4bb6377e0017505f7c9d6b2
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-02-25 11:54:18 +0530

    updated

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //your code here
-function setClock() {
+function updateClock() {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-
-    const hourAngle = (hours / 12) * 360 + (minutes / 60) * 30 - 90;
-    const minuteAngle = (minutes / 60) * 360 + (seconds / 60) * 6 - 90;
-    const secondAngle = (seconds / 60) * 360 - 90;
-
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+    
     const hourHand = document.querySelector('.hour-hand');
     const minuteHand = document.querySelector('.minute-hand');
     const secondHand = document.querySelector('.second-hand');
-
+    
+    const hourAngle = (hour % 12) * 30 + (minute / 60) * 30;
+    const minuteAngle = minute * 6;
+    const secondAngle = second * 6;
+    
     hourHand.style.transform = `rotate(${hourAngle}deg)`;
     minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
     secondHand.style.transform = `rotate(${secondAngle}deg)`;
-}
-
-setInterval(setClock, 1000);
+  }
+  
+  setInterval(updateClock, 1000);
